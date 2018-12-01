33c180fd23203403c27cc979712eea8de6675741
Brain <Brian@BraindeMacBook-Pro.local>
2018-12-01 05:14:50 -0800

    Intergation

--- angular_version/BeaverRider/src/app/user-center/user-center.component.ts
+++ angular_version/BeaverRider/src/app/user-center/user-center.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-center.component.css']
 })
 export class UserCenterComponent implements OnInit {
+  switch_flag: number = 1;
+  driver_active: string = 'active';
+  passenger_active: string = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  switch_passenger(): void{
+    this.switch_flag = 0;
+    this.driver_active = '';
+    this.passenger_active = 'active';
+  }
+
+  switch_driver(): void{
+    this.switch_flag = 1;
+    this.driver_active = 'active';
+    this.passenger_active = '';
   }
 
 }
