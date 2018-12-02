49eabf5ddf7abb3b33abd4a337562995c093f987
SuerpX <SuerpX@users.noreply.github.com>
2018-12-02 00:30:29 -0800

    fixed bugs for match api and test all API pass

--- test/test_connectOrder.php
+++ test/test_connectOrder.php
@@ -260,7 +260,7 @@
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function()
-    {
+    { 
         if( xhr.readyState==4 && xhr.status==200 )
         {
             console.log(xhr.responseText);
